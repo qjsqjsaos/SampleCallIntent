c8f32fa7e10ba363a6fb1c86a224f33c6d341b56
Beaksooyeol <merrygoaround0726@gmail.com>
2020-12-28 10:45:21 +0900

    clear.

--- app/src/main/java/org/techtown/samplecallintent/MainActivity.java
+++ app/src/main/java/org/techtown/samplecallintent/MainActivity.java
@@ -2,6 +2,7 @@ package org.techtown.samplecallintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -30,6 +31,18 @@ public class MainActivity extends AppCompatActivity {
                 //확실하지는 않지만, 와이파이에서는 잘 안됬고, 랜선에서 잘된거 보니,
                 //이 차이로 인한 영향일 수도 있다는 유추를 해볼 수 있다.
                 //이 원인을 알기에 내 역량은 아직 부족하므로, 더 성장하고 와서 다시 살펴보겠다.
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                ComponentName name = new ComponentName("org.techtown.samplecallintent", "org.techtown.samplecallintent.MenuActivity");
+                //컴포넌트 이름을 지정할 수 있는 객체 생성
+                intent.setComponent(name); //인텐트 객체에 컴포넌트 지정
+                startActivityForResult(intent, 101); //액티비티 띄우기
             }
         });
     }
